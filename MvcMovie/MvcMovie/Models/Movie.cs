eb4a628f831dfcdbffc5302beb5f30863c8e2748
river.jiang <riverloner@163.com>
2012-12-07 22:11:33 +0800

    Adding a model Entity framework code first.

---
+++ MvcMovie/MvcMovie/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public virtual int Id { get; set; }
+        public virtual string Title { get; set; }
+        public virtual DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}

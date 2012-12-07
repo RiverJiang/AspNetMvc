650444fa849a82886d4b063a82346e17281e20b5
river.jiang <riverloner@163.com>
2012-12-07 21:01:20 +0800

    Add a project MvcMovie Add a controller let us know the url and controller routing mechanism.

---
+++ MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HellWorld/
+
+        public string Index()
+        {
+            return "This is my <b>Default</b> action...";
+        }
+
+        //
+        // GET: /HelloWorld/Welcome/
+        public string Welcome(string name,int numTimes =1)
+        {
+            return HttpUtility.HtmlEncode("Hello" + name + ", NumTimes is:" + numTimes);
+        }
+    }
+}

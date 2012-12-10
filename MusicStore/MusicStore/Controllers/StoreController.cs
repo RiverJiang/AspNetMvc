2eb2422c29eae4628b0389cf9a9f607c37357cd5
river.jiang <riverloner@163.com>
2012-12-10 21:31:23 +0800

    Add a new mvc sample music store add the first controller and configure the layout.

---
+++ MusicStore/MusicStore/Controllers/StoreController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        //
+        // GET: /Store/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Store/
+
+        public ActionResult Browse()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Store/
+
+        public ActionResult Details()
+        {
+            return View();
+        }
+    }
+}

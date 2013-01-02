077bd953b075a09bc5f36d32f57da266f793a41c
river.jiang <riverloner@163.com>
2013-01-02 14:29:29 +0800

    update music store.

--- MusicStore/MusicStore/Global.asax.cs
+++ MusicStore/MusicStore/Global.asax.cs
@@ -11,17 +11,18 @@ namespace MusicStore
 {
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
     // visit http://go.microsoft.com/?LinkId=9394801
+
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
-            System.Data.Entity.Database.SetInitializer(new MusicStore.Models.SampleData());
             AreaRegistration.RegisterAllAreas();
 
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            AuthConfig.RegisterAuth();
         }
     }
 }
--- MusicStore/MusicStore/Properties/AssemblyInfo.cs
+++ MusicStore/MusicStore/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("43db7a40-7cd0-4b6b-8cf9-5aaa1479e599")]
+[assembly: Guid("3b52d8c4-a273-4783-8f9d-867cab625722")]
 
 // Version information for an assembly consists of the following four values:
 //

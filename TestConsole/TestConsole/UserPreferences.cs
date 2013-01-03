5a8950ff6a8553720ca0618ddafcbc0a572bef28
river.jiang <riverloner@163.com>
2013-01-03 22:02:05 +0800

    static constructor example.

---
+++ TestConsole/TestConsole/UserPreferences.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    public class UserPreferences
+    {
+        public static readonly Color BackColor;
+
+        static UserPreferences()
+        {
+            DateTime now = DateTime.Now;
+
+            if (now.DayOfWeek == DayOfWeek.Saturday || now.DayOfWeek == DayOfWeek.Sunday)
+            {
+                BackColor = Color.Green;
+            }
+            else
+            {
+                BackColor = Color.Red;
+            }
+        }
+
+        private UserPreferences()
+        {
+        }
+    }
+}
